40f68e1860361cc42938549b1fff487896c9f94c
GaganTut <gstut@live.com>
2017-03-20 14:42:12 -1000

    Passed the set test

--- linkedList.js
+++ linkedList.js
@@ -5,25 +5,48 @@
  */
 function linkedListGenerator(){
 
-  var newList = {
-    value: "First Value",
-    next: null
-  };
+  var linkHead = null;
+  var linkTail = null;
 
   function getHead() {
-    return newList.value;
+    return linkHead;
   }
 
   function getTail() {
-
+    return linkTail;
   }
 
   function add(Value) {
+    var newNode = {
+      value : Value,
+      next : null
+    };
+    if (getHead() === null) {
+      linkHead = newNode;
+      linkTail = newNode;
 
+    } else if (linkHead.next === null) {
+      linkTail = newNode;
+      linkHead.next = linkTail;
+    } else {
+      linkTail.next = newNode;
+      linkTail = newNode;
+    }
+
+    return newNode;
   }
 
-  function get(Number) {
 
+  function get(Number) {
+    var getNode = linkHead;
+    for (var i = 0; i < Number; i ++) {
+      if (getNode.next === null) {
+        return false;
+      } else {
+        getNode = getNode.next;
+      }
+    }
+    return getNode;
   }
 
   function remove(Number) {
